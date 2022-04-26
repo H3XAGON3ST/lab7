f76d6a0df967adcc77f2d05079fb91c002a894af
Vtorushin Nikita <h3xagon3stofficial@gmail.com>
2022-04-26 14:52:32 +0500

    Добавьте файлы проекта.

---
+++ lab4/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IWebDriver driver = new ChromeDriver();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            driver.Navigate().GoToUrl("https://vk.com/");
+            driver.FindElement(By.XPath("//*[@id=\"index_email\"]")).SendKeys(Login.Text);
+            driver.FindElement(By.XPath("//*[@id=\"index_pass\"]")).SendKeys(Password.Password);
+            driver.FindElement(By.XPath("//*[@id=\"index_login_button\"]")).Click();
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+            driver.Close();
+            driver.Dispose();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+
+            var stream = client.OpenRead(source.Text);
+
+            var htmlText = "";
+            
+            using (StreamReader textReader = new StreamReader(stream, Encoding.UTF8, true))
+            {
+                htmlText = textReader.ReadToEnd();
+            }
+            File.WriteAllText("page.html", htmlText);
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            webbros.Navigate(source.Text);
+        }
+    }
+}
